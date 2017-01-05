90227df9146fdeaf4561e6745307620c62e392cc
shorfng <shorfng@126.com>
2017-01-05 11:35:35 +0800

    0.创建项目

---
+++ 百思不得姐/百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by 蓝田 on 2017/1/5.
+//  Copyright © 2017年 蓝田. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 百思不得姐/百思不得姐/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  百思不得姐
+//
+//  Created by 蓝田 on 2017/1/5.
+//  Copyright © 2017年 蓝田. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
